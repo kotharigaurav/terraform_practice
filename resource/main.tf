95e3b552e6c256a1ec5013f19949fad5080028e9
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-03-16 01:25:32 +0530

    adding code for s3 bucket creation

--- resource/main.tf
+++ resource/main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "gaurav-terraform-testing-bucket"
+# resource "aws_s3_bucket" "example" {
+#   bucket = "gaurav-terraform-testing-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
