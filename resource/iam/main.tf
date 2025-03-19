2fd448a373c681d61f3eaa479d8341a4e2bb0f20
Gaurav Kothari <kotharigaurav3699@gmail.com>
2025-03-20 01:40:29 +0530

    adding resoucre for iam policy attachment

--- resource/iam/main.tf
+++ resource/iam/main.tf
@@ -2,8 +2,8 @@
 # IAM Role
 ###################################
 
-module "iam_role_module" {
-  source    = "../../modules/iam"
-  role_name = local.iam_role_name
-  policy_name = local.iam_policy_name
-}
+# module "iam_role_module" {
+#   source    = "../../modules/iam"
+#   role_name = local.iam_role_name
+#   policy_name = local.iam_policy_name
+# }
